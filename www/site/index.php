f728fe99557245a2973004b0828412898c9a719d
andreashovland <andreashovland96@gmail.com>
2021-10-25 19:32:19 +0200

    tilkobling til databasen

--- www/site/index.php
+++ www/site/index.php
@@ -1,6 +1,8 @@
 <?php
 include("inc/config.php");
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
         echo "Connection failed: " . $conn->connect_error;
     } else {
