1504ce478824e2077a0cedbcbc2e00eabee04bbb
andreashovland <andreashovland96@gmail.com>
2021-10-27 19:05:48 +0200

    lagt til tabell

--- www/site/index.php
+++ www/site/index.php
@@ -9,11 +9,42 @@
 <?php
 include("inc/config.php");
 include("inc/navbar.php");
-
-if ($conn->connect_error) {
-        echo "Connection failed: " . $conn->connect_error;
-    } else {
-    }
 ?>
+    
+    <div class='container'>
+        <table>
+        <tr>
+            <th>Medlem ID</th>
+            <th>Fornavn</th>
+            <th>Etternavn</th>
+            <th>Adresse</th>
+            <th>Postnummer</th>
+            <th>Poststed</th>
+            <th>Mobil</th>
+            <th>E-post</th>
+            <th>Født</th>
+            <th>Kjønn</th>
+            <th>Interesser</th>
+            <th>Medlem siden</th>
+            <th>Kontingentstatus</th>
+            <th>Rolle</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Andreas</td>
+            <td>Martinsen</td>
+            <td>Verven 4</td>
+            <td>4608</td>
+            <td>Kristiansand</td>
+            <td>91008947</td>
+            <td>andreas@outlook.com</td>
+            <td>08.09.1996</td>
+            <td>Gutt</td>
+            <td>Fotball</td>
+            <td>10.05.2021</td>
+            <td>Betalt</td>
+            <td>Trener</td>
+        </tr>
+        </table>
 </body>
 </html>
