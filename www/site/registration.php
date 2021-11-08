40cba07d739b37387108fcb78d615797fe7ca128
Eliasgauslaa <elias--g@hotmail.com>
2021-11-08 18:14:36 +0100

    prøver igjen for helvette

--- www/site/registration.php
+++ www/site/registration.php
@@ -20,43 +20,40 @@
             <div class="row">
                 <div class="col-sm-3">
                 <h1>Registration</h1>
-                <p>Fill in the form to register</p>
+                <p>Fyll inn for å registrere et medlem</p>
 
-                <label for="fnavn"><b>First name</b></label>
+                <label for="fnavn"><b>Fornavn</b></label>
                 <input class="form-control" type="text" name="fnavn" placeholder="Fornavn" required><br>
 
-                <label for="enavn"><b>Last name</b></label>
+                <label for="enavn"><b>Etternavn</b></label>
                 <input class="form-control" type="text" name="enavn" placeholder="Etternavn" required><br>
 
-                <label for="adresse"><b>Adress</b></label>
+                <label for="adresse"><b>Adresse</b></label>
                 <input class="form-control" type="text" name="adresse" placeholder="Gateadresse" required><br>
     
-                <label for="pnummer"><b>Postal code</b></label>
+                <label for="pnummer"><b>Postnummer</b></label>
                 <input class="form-control" type="int" name="pnummer" placeholder="Postnummer" required><br>
-                
-                <label for="kjønn"><b>Gender</b></label>
-                <input type="radio" name="kjønn" value="Mann"> Male
-                <input type="radio" name="kjønn" value="Kvinne"> Female<br>
 
-                <label for="email"><b>E-mail</b></label>
+                <label for="postAdresse"><b>Postadresse</b></label>
+                <input class="form-control" type="text" name="postadresse" placeholder="Postadresse" required><br>
+
+                <label for="tlf"><b>Telefon</b></label>
+                <input class="form-control" type="tel" name="tlf" placeholder="Mobilnummer" required><br>
+                
+                <label for="email"><b>E-post</b></label>
                 <input class="form-control" type="email" name="epost" placeholder="E-post" required><br>
 
-                <label for="tlf"><b>Phone number</b></label>
-                <input class="form-control" type="tel" name="tlf" placeholder="Mobilnummer" required><br>
-
-                <label for="fdato"><b>Date of birth</b></label>
-                <input type="date" name="fdato" value="1998-05-05" required><br>
+                <label for="fdato"><b>Fødselsdato</b></label>
+                <input type="date" name="fdato" value="" required><br>
+                                
+                <label for="kjønn"><b>Kjønn</b></label>
+                <input type="radio" name="kjønn" value="1"> Male
+                <input type="radio" name="kjønn" value="2"> Female<br>
                 
-                <label for="mdato"><b>Member since</b></label>
-                <input type="date" name="mdato" value="2020-04-10" required><br>
-
-                <label for="interesser"><b>Interests</b></label>
+                <label for="interesser"><b>Interesser</b></label>
                 <input class="form-control" type="text" name="interesser" placeholder="Interesser"><br>
 
-                <label for="kaktiviteter"><b>Course activities</b></label>
-                <input class="form-control" type="text" name="kaktiviteter" placeholder="Kursaktiviteter"><br>
-
-                <label for="betalt"><b>Contingent Status</b></label>
+                <label for="betalt"><b>Kontigentstatus</b></label>
                 <input type="radio" name="betalt" value="betalt"> Paid
                 <input type="radio" name="betalt" value="ikkeBetalt"> Not Paid<br>
                 
