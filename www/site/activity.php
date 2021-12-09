b92e8fa7a8d9ab7218d4958fe014e96b50128f0a
andreashovland <andreashovland96@gmail.com>
2021-12-09 17:49:32 +0100

    kommentarer

--- www/site/activity.php
+++ www/site/activity.php
@@ -2,11 +2,14 @@
 
 include("inc/header.php");
 
+// henter activityID fra tabellen
 $activityID = $_GET['activityID'];
 
+// henter data fra databasen 
 $membersSelect = $conn->query("SELECT * FROM member");
 $activitiesSelect = $conn->query("SELECT * FROM activities WHERE activityID = $activityID");
 
+// henter verdiene fra radene og plasserer de i variabler
 $activitiesRows = $activitiesSelect->fetch_assoc();
 
 $activity = $activitiesRows['activity'];
@@ -15,28 +18,34 @@ $date = $activitiesRows['date'];
 $start = $activitiesRows['start'];
 $end = $activitiesRows['end'];
 
+// Skriver ut informasjon om aktiviteten
 echo "<h1>". $activity . " , " . $date . " kl. ". substr($start,0,5) . " - " . substr($end,0,5) . "</h1>";
 echo "<h2>Ansvarlig: $responsible </h2>";
 
 if(isset($_POST['add'])){
     
+    // memberID hentes fra valgt medlem i dropdown
     $memberID = $_POST['member'];
 
+    // Legger valgt memberID og activityID inn i memberactivities
     $query = "INSERT INTO memberactivities (memberID, activityID)
     VALUES ($memberID, $activityID)";
 
+    // if else til å sjekke om medlemmet ble meldt på eller om det allerede er påmeldt
     if(mysqli_query($conn, $query)){
         echo "<h3>Valgt medlem er nå påmeldt. </h3>"; 
     } else{
         echo "Medlemmet er allerede påmeldt. ";
     }
 
+    // Select-statement for å hente ut alle medlemmer som er meldt på en viss aktivitet
     $signedup = "SELECT firstName, lastName FROM member
     JOIN memberactivities ON memberactivities.memberID=member.memberID
     WHERE activityID = $activityID";
         
     $signedupResult = $conn->query($signedup);
 
+    // Tabellen skrives ut
     echo "<div class='container'>
     <table>
     <tr>
